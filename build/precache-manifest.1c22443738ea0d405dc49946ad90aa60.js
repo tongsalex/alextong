fcf13c149f9cd43e7216b2575469fc671a6d46e0
Alex Tong <tongsalex@gmail.com>
2020-05-03 18:46:42 -0400

    adding build folder

---
+++ build/precache-manifest.1c22443738ea0d405dc49946ad90aa60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ff0029f0aaac93a1810a983aca6399f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d07e4ffa107e0187660a",
+    "url": "/static/css/main.18d19359.chunk.css"
+  },
+  {
+    "revision": "8805981f7c42f7ea601a",
+    "url": "/static/js/2.1b69760a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1b69760a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d07e4ffa107e0187660a",
+    "url": "/static/js/main.c3170a87.chunk.js"
+  },
+  {
+    "revision": "1c464e5d58e86882fe5a",
+    "url": "/static/js/runtime-main.7620469c.js"
+  },
+  {
+    "revision": "d0757ac43701ff4c7e96676c19263287",
+    "url": "/static/media/Signerica_Fat.d0757ac4.ttf"
+  }
+]);
