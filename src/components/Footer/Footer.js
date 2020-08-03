f1c33c5bc297b80a6f17587232da8b51354268fa
Alex Tong <tongsalex@gmail.com>
2020-08-03 11:45:53 -0400

    added project descriptions

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,32 +5,35 @@ import github from '../../images/github.png';
 import linkedin from '../../images/linkedin.png';
 import angellist from '../../images/angellist.png';
 import medium from '../../images/medium.png';
+import robinhood from '../../images/robinhood.jpg';
 import './Footer.css'
 
 
 const Footer = props => (
   <nav className="footer">
     <div className="footer--left">
-
     </div>
 
     <div className="footer--right">
-      <a href='http://github.com/tongsalex'>
+      <a href='http://github.com/tongsalex' target="_blank" rel="noopener noreferrer">
         <img src={github} alt='github' className="button hover-shadow" />
       </a>
-      <a href='http://linkedin.com/in/tongsalex'>
+      <a href='http://linkedin.com/in/tongsalex' target="_blank" rel="noopener noreferrer">
         <img src={linkedin} alt='linkedin' />
       </a>
-      <a href='http://angel.co/tongsalex'>
+      <a href='http://angel.co/tongsalex' target="_blank" rel="noopener noreferrer">
         <img src={angellist} alt='angellist' />
       </a>
-      <a href='http://medium.com/@tongsalex'>
+      <a href='http://medium.com/@tongsalex' target="_blank" rel="noopener noreferrer">
         <img src={medium} alt='medium' />
       </a>
-      <a href='http://wlth.fr/1R8MPJy'>
+      <a href='http://wlth.fr/1R8MPJy' target="_blank" rel="noopener noreferrer">
         <img src={wealthfront} alt='wealthfront' />
       </a>
-      <a href='http://soundcloud.com/tongsalex'>
+      <a href='https://join.robinhood.com/alexant2910' target="_blank" rel="noopener noreferrer">
+        <img src={robinhood} alt='robinhood' />
+      </a>
+      <a href='http://soundcloud.com/tongsalex' target="_blank" rel="noopener noreferrer">
         <img src={soundcloud} alt='soundcloud' />
       </a>
 
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -25,6 +25,7 @@ const Projects = () => (
 
     <div className="projects__container projects__container--noteworthy" style={{ backgroundImage:`url(${noteworthy})`}}>
       <h1 className="projects__title">noteworthy</h1>
+      <h2 className="projects__description">A full-stack note-taking app modeled after Evernote built using Ruby on Rails, React, Redux, PostgreSQL and bcrypt for user authentication.</h2>
       <div className="projects__linkContainer">
         <a href='http://www.github.com/tongsalex/noteworthy' target="_blank" rel="noopener noreferrer">
           <img src={github} alt='github' className="projects__link" />
@@ -39,6 +40,7 @@ const Projects = () => (
 
     <div className="projects__container projects__container--noteworthy" style={{ backgroundImage:`url(${kollectiv})`}}>
       <h1 className="projects__title">kollectiv</h1>
+      <h2 className="projects__description">A full-stack blog on New York City's music, food, fashon & events built using PostgreSQL, Express, React, Node and JSON Web Tokens for user authentication.</h2>
       <div className="projects__linkContainer">
         <a href='http://www.github.com/tongsalex/kollectiv' target="_blank" rel="noopener noreferrer">
           <img src={github} alt='github' className="projects__link" />
@@ -53,6 +55,7 @@ const Projects = () => (
     
     <div className="projects__container projects__container--noteworthy" style={{ backgroundImage:`url(${bargain})`}}>
       <h1 className="projects__title">BarGain</h1>
+      <h2 className="projects__description">A full-stack marketplace platform for users to buy and sell items based on their geolocation, built using the Google Maps API, MongoDB, Express, Node, EJS, HTML, CSS and bcryptjs for user authentication.</h2>
       <div className="projects__linkContainer">
         <a href='http://www.github.com/tongsalex/bargain' target="_blank" rel="noopener noreferrer">
           <img src={github} alt='github' className="projects__link" />
@@ -65,6 +68,7 @@ const Projects = () => (
 
     <div className="projects__container projects__container--noteworthy" style={{ backgroundImage:`url(${constellations})`}}>
       <h1 className="projects__title">constellations</h1>
+      <h2 className="projects__description">A 3D JavaScript simulation of space built primarily on Three.js.</h2>
       <div className="projects__linkContainer">
         <a href='http://www.github.com/tongsalex/constellations' target="_blank" rel="noopener noreferrer">
           <img src={github} alt='github' className="projects__link" />
@@ -76,7 +80,8 @@ const Projects = () => (
     </div>
 
     <div className="projects__container projects__container--noteworthy" style={{ backgroundImage:`url(${roadtorock})`}}>
-      <h1 className="projects__title">roadToRock</h1>
+      <h1 className="projects__title">Road To Rock</h1>
+      <h2 className="projects__description">A classic rock music interactive trivia game built using solely HTML, CSS, JavaScript & jQuery. My very first web application!</h2>
       <div className="projects__linkContainer">
         <a href='http://www.github.com/tongsalex/RoadToRock' target="_blank" rel="noopener noreferrer">
           <img src={github} alt='github' className="projects__link" />
@@ -90,7 +95,8 @@ const Projects = () => (
     <h1 className="projects__divider projects__divider--collab">COLLABORATIVE PROJECTS</h1>
 
     <div className="projects__container projects__container--noteworthy" style={{ backgroundImage:`url(${vitaldb})`}}>
-      <h1 className="projects__title">vitalDB</h1>
+      <h1 className="projects__title">Vital DB</h1>
+      <h2 className="projects__description">A health vital stats tracker allowing users to centralize their medical data built using MongoDB, Express, React & Node.</h2>
       <div className="projects__linkContainer">
         <a href='http://www.github.com/tongsalex/vitaldb' target="_blank" rel="noopener noreferrer">
           <img src={github} alt='github' className="projects__link" />
@@ -102,12 +108,12 @@ const Projects = () => (
     </div>
 
     <div className="projects__container projects__container--noteworthy" style={{ backgroundImage:`url(${pbandjamd})`}}>
-      <h1 className="projects__title">PBandJamd</h1>
+      <h1 className="projects__title">PB and Jamd</h1>
+      <h2 className="projects__description">A full-stack live forum for users to create and comment on any topic they please, with real-time dynamic updating. Built on PostgreSQL, Express, React & Node.</h2>
       <div className="projects__linkContainer">
         <a href='http://www.github.com/tongsalex/pbandjamd' target="_blank" rel="noopener noreferrer">
           <img src={github} alt='github' className="projects__link" />
         </a>
-
         <a href='http://www.tongsalex.com/pbandjamd' target="_blank" rel="noopener noreferrer">
           <img src={live} alt='live' className="projects__link" />
         </a>
@@ -118,6 +124,7 @@ const Projects = () => (
     {/* // https://vpay-frontend.herokuapp.com/ */}
     <div className="projects__container projects__container--noteworthy" style={{ backgroundImage:`url(${vpay})`}}>
       <h1 className="projects__title">vPay</h1>
+      <h2 className="projects__description">A full-stack live forum for users to create and comment on any topic they please, with real-time dynamic updating. Built on PostgreSQL, Express, React & Node.</h2>
       <div className="projects__linkContainer">
         <a href='https://github.com/thomas-westfall/vpay' target="_blank" rel="noopener noreferrer">
           <img src={github} alt='github' className="projects__link" />
@@ -129,7 +136,8 @@ const Projects = () => (
     </div>
 
     <div className="projects__container projects__container--elephant" style={{ backgroundImage:`url(${elephant})`}}>
-      <h1 className="projects__title">PreventElephantPoaching</h1>
+      <h1 className="projects__title">Prevent Elephant Poaching</h1>
+      <h2 className="projects__description">A donation website to prevent elephant poaching built on HTML and CSS.</h2>
       <div className="projects__linkContainer">
         <a href='https://github.com/tongsalex/preventElephantPoaching' target="_blank" rel="noopener noreferrer">
           <img src={github} alt='github' className="projects__link" />
